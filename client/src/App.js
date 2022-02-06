3de1625ad65d4e0876565cc3da979928646b1717
Sachin Kamath <kamathsachin@hotmail.com>
2022-02-06 09:13:39 -0600

    Added Create Post Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import PostCreate from './PostCreate';
 
 const App = () => {
-  return <div>My Blog Post</div>;
+  return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreate />
+    </div>
+  );
 };
 
 export default App;
